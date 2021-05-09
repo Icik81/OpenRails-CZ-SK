f610235b9c3f576bc420d0be443ec12c8245fee7
Icik81 <78701635+Icik81@users.noreply.github.com>
2021-05-09 15:54:23 +0200

    Optimalizace AirSinglePipe

--- Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Brakes/BrakeSystem.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Brakes/BrakeSystem.cs
@@ -84,6 +84,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Brakes
         public bool BailOffOnAntiSkid;
         public bool IsAirEmpty;
         public bool HandBrakeActive;
+        public bool StartOn = true;
 
         /// <summary>
         /// Front brake hoses connection status
