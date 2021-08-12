4ec62c5c7608bca4edfc83580e6ff21428d344ea
Icik81 <78701635+Icik81@users.noreply.github.com>
2021-08-12 20:35:38 +0200

    Update InputSettings.cs

--- Source/ORTS.Settings/InputSettings.cs
+++ Source/ORTS.Settings/InputSettings.cs
@@ -550,7 +550,7 @@ namespace ORTS.Settings
             // Icik
             Commands[(int)UserCommand.ControlCompressorMode_OffAuto] = new UserCommandKeyInput(0x2E, KeyModifiers.Control);
             Commands[(int)UserCommand.ControlHeating_OffOn] = new UserCommandKeyInput(0x23, KeyModifiers.Control);
-            Commands[(int)UserCommand.ControlSwitchingVoltageMode_OffDC] = new UserCommandKeyInput(0x1F, KeyModifiers.Shift);
+            Commands[(int)UserCommand.ControlSwitchingVoltageMode_OffDC] = new UserCommandKeyInput(0x1F, KeyModifiers.Shift | KeyModifiers.Control);
             Commands[(int)UserCommand.ControlSwitchingVoltageMode_OffAC] = new UserCommandKeyInput(0x1F, KeyModifiers.Control);
         }
 #endregion
