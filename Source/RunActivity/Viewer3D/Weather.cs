ef3725a3fc8efa249d0a17ecd17c34e34c69b82e
Icik81 <78701635+Icik81@users.noreply.github.com>
2022-01-03 22:19:16 +0100

    Update Weather.cs

--- Source/RunActivity/Viewer3D/Weather.cs
+++ Source/RunActivity/Viewer3D/Weather.cs
@@ -263,9 +263,9 @@ namespace Orts.Viewer3D
             // These values are defaults only; subsequent changes to the weather via debugging only change the components (weather, overcastFactor and fogDistance) individually.
             switch (Viewer.Simulator.WeatherType)
             {
-                case Orts.Formats.Msts.WeatherType.Clear: Weather.OvercastFactor = 0.05f; Weather.FogDistance = 20000; break;
-                case Orts.Formats.Msts.WeatherType.Rain: Weather.OvercastFactor = 0.7f; Weather.FogDistance = 1000; break;
-                case Orts.Formats.Msts.WeatherType.Snow: Weather.OvercastFactor = 0.6f; Weather.FogDistance = 500; break;
+                case Orts.Formats.Msts.WeatherType.Clear: Weather.OvercastFactor = 0.05f; Weather.FogDistance = 20000; Weather.PricipitationIntensityPPSPM2 = 0f; break;
+                case Orts.Formats.Msts.WeatherType.Rain: Weather.OvercastFactor = 0.7f; Weather.FogDistance = 1000; Weather.PricipitationIntensityPPSPM2 = 0.7500f; break;
+                case Orts.Formats.Msts.WeatherType.Snow: Weather.OvercastFactor = 0.6f; Weather.FogDistance = 500; Weather.PricipitationIntensityPPSPM2 = 0.7500f; break;
             }
         }
 
