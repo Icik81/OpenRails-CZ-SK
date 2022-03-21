55b2c05fa863e8158a6791e00e5d804391304564
Icik81 <78701635+Icik81@users.noreply.github.com>
2022-03-21 20:22:43 +0100

    Oprava dynamický náklad

--- Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/SubSystems/Controllers/MSTSNotchController.cs
@@ -141,7 +141,7 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
         public float MaximumValue = 1;
         public const float StandardBoost = 5.0f; // standard step size multiplier
         public const float FastBoost = 20.0f;
-        public float StepSize;
+        public float StepSize = 0.01f;
         private List<MSTSNotch> Notches = new List<MSTSNotch>();
         public int CurrentNotch { get; set; }
         public bool ToZero = false; // true if controller zero command;
@@ -424,6 +424,9 @@ namespace Orts.Simulation.RollingStocks.SubSystems.Controllers
 
         private float UpdateValues(float elapsedSeconds, float direction, float boost)
         {
+            // Icik             
+            StepSize = MathHelper.Clamp(StepSize, 0.01f, 1);
+
             //We increment the intermediate value first
             IntermediateValue += StepSize * elapsedSeconds * GetNotchBoost(boost) * direction;
             IntermediateValue = MathHelper.Clamp(IntermediateValue, MinimumValue, MaximumValue);
