c9d5ffc84ae7d9b8f57343eb12ecd1c7e113c280
Icik81 <78701635+Icik81@users.noreply.github.com>
2022-07-31 14:54:10 +0200

    Update MSTSWagonViewer.cs

--- Source/RunActivity/Viewer3D/RollingStock/MSTSWagonViewer.cs
+++ Source/RunActivity/Viewer3D/RollingStock/MSTSWagonViewer.cs
@@ -1122,6 +1122,10 @@ namespace Orts.Viewer3D.RollingStock
                     smsGenericFilePath = "..\\Content\\GenericSound\\6_Wheels\\GenSound_ex.sms";
                     break;
             }
+            // Icik
+            if (MSTSWagon.CarSoundLoaded == false)
+                Viewer.SoundProcess.AddSoundSource(this, new SoundSource(Viewer, MSTSWagon, System.IO.Path.Combine(Viewer.ContentPath, smsGenericFilePath)));
+            MSTSWagon.CarSoundLoaded = true;
 
             if (!File.Exists(smsFilePath))
                 smsFilePath = Viewer.Simulator.BasePath + @"\sound\" + filename;
@@ -1133,11 +1137,7 @@ namespace Orts.Viewer3D.RollingStock
 
             try
             {
-                Viewer.SoundProcess.AddSoundSource(this, new SoundSource(Viewer, MSTSWagon, smsFilePath));
-                // Icik
-                if (MSTSWagon.CarSoundLoaded == false)
-                    Viewer.SoundProcess.AddSoundSource(this, new SoundSource(Viewer, MSTSWagon, System.IO.Path.Combine(Viewer.ContentPath, smsGenericFilePath)));
-                MSTSWagon.CarSoundLoaded = true;
+                Viewer.SoundProcess.AddSoundSource(this, new SoundSource(Viewer, MSTSWagon, smsFilePath));                
             }
             catch (Exception error)
             {
