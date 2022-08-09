e0e375809aa38a8f166021f701f968b178e71eef
Icik81 <78701635+Icik81@users.noreply.github.com>
2022-08-09 20:07:11 +0200

    Update Simulator.cs

--- Source/Orts.Simulation/Simulation/Simulator.cs
+++ Source/Orts.Simulation/Simulation/Simulator.cs
@@ -1300,17 +1300,7 @@ namespace Orts.Simulation
             if (conFileName.Contains("tilted")) train.IsTilting = true;
 
             // Icik
-            /*
-            if (conFileName.Contains("airempty")) train.LocoIsAirEmpty = true;
-            if (conFileName.Contains("AirEmpty")) train.LocoIsAirEmpty = true;
-            if (conFileName.Contains("airEmpty")) train.LocoIsAirEmpty = true;
-            if (conFileName.Contains("Airempty")) train.LocoIsAirEmpty = true;
-            if (conFileName.Contains("AirE")) train.LocoIsAirEmpty = true;
-            if (conFileName.Contains("airE")) train.LocoIsAirEmpty = true;
-            if (conFileName.Contains("Aire")) train.LocoIsAirEmpty = true;
-            */
-
-            if ((conFileName.ToLower().Contains("airempty") || conFileName.ToLower().Contains("aire")) && !conFileName.Contains("aire")) train.LocoIsAirEmpty = true;
+            if ((conFileName.ToLower().Contains("airempty") || conFileName.ToLower().Contains("aire")) && !conFileName.Contains("aire")) Settings.AirEmpty = true;
 
 
 #if ACTIVITY_EDITOR
