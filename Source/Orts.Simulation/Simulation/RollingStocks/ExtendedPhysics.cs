9f42e0ba3b55d021d387cc6f4468fcaa7d215a5b
Icik81 <78701635+Icik81@users.noreply.github.com>
2023-02-21 20:52:33 +0100

    Update ExtendedPhysics.cs

--- Source/Orts.Simulation/Simulation/RollingStocks/ExtendedPhysics.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/ExtendedPhysics.cs
@@ -502,7 +502,7 @@ namespace Orts.Simulation.RollingStocks
                     if (FastestAxleSpeedMpS < ea.WheelSpeedMpS)
                         FastestAxleSpeedMpS = ea.WheelSpeedMpS;
 
-                    speedDiff = (ea.WheelSpeedMpS - myAverageAxleSpeedMps) * 10.0f;
+                    speedDiff = (ea.WheelSpeedMpS - myAverageAxleSpeedMps) * 1.0f;
                     if (speedDiff < 0)
                         speedDiff = 0;
                     if (OverridenControllerVolts - speedDiff < 0)
