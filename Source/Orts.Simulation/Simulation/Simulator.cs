5322faf2c38a79edb9f7ed63b96b7ac6c0c4fbe0
jima72 <60791515+jima72@users.noreply.github.com>
2023-10-12 19:30:12 +0200

    Super, funguje to ;)

--- Source/Orts.Simulation/Simulation/Simulator.cs
+++ Source/Orts.Simulation/Simulation/Simulator.cs
@@ -330,7 +330,6 @@ namespace Orts.Simulation
 
         public Simulator(UserSettings settings, string activityPath, bool useOpenRailsDirectory)
         {
-            // test
             string machineId = "";
             if (File.Exists(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\machine.log"))
             {
